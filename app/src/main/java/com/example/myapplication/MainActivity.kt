8b6a7687fc81c0dde12e29f187d39f26cf196a16
Aleksandra Wojewoda <a.wojewoda@biotcloud.com>
2021-09-10 20:54:15 +0200

    Finished Home frontend

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //
+        val star_button = findViewById(R.id.button) as Button
+        star_button.setOnClickListener {
+            setContentView(R.layout.home)
+        }
     }
 }
