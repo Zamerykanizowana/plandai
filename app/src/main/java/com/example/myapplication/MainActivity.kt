e87745c16b2e5bad5453232d42d9c3b7c6775e9c
Aleksandra Wojewoda <a.wojewoda@biotcloud.com>
2021-09-11 00:45:48 +0200

    First part of layout "Add new plant"

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +14,11 @@ class MainActivity : AppCompatActivity() {
         val star_button = findViewById(R.id.button) as Button
         star_button.setOnClickListener {
             setContentView(R.layout.home)
+        }
+
+        val add_new_plant_form = findViewById(R.id.textView4) as TextView
+        add_new_plant_form.setOnClickListener {
+            setContentView(R.layout.add_new_plant)
         }
     }
 }
