421b4e52a0d45c8751188909fd4de8b0f5f5a91a
Adam Olech <nddr89@gmail.com>
2021-09-12 17:07:52 +0200

    Use intent to enter AddNewPlant activity

---
+++ app/src/main/java/com/example/myapplication/AddNewPlantActivity.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.util.Log
+
+class AddNewPlantActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.add_new_plant)
+    }
+}
