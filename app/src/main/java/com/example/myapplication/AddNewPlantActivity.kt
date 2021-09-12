24efb1f746e6d4a5027e51ea44d8cedaf34e060e
Adam Olech <nddr89@gmail.com>
2021-09-12 19:29:55 +0200

    AddNewPlantActivity: attach function to homeBtn onClick event listener

--- app/src/main/java/com/example/myapplication/AddNewPlantActivity.kt
+++ app/src/main/java/com/example/myapplication/AddNewPlantActivity.kt
@@ -1,14 +1,20 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.util.Log
+import android.view.View
 
 class AddNewPlantActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_new_plant)
+    }
+
+    fun goHome(view: View) {
+        startActivity(Intent(this, HomeActivity::class.java))
     }
 }
