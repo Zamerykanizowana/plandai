1f7d95eabd4b1670cec3a0ed600795088e932a47
Aleksandra Wojewoda <a.wojewoda@biotcloud.com>
2021-09-12 23:27:39 +0200

    AddNewPlantActivity: fix submit button

--- app/src/main/java/com/example/myapplication/AddNewPlantActivity.kt
+++ app/src/main/java/com/example/myapplication/AddNewPlantActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.TextView
 import android.util.Log
 import android.view.View
+import android.widget.EditText
 
 class AddNewPlantActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,8 +15,11 @@ class AddNewPlantActivity : AppCompatActivity() {
         setContentView(R.layout.add_new_plant)
     }
 
-    fun addNewPlant() {
-
+    fun addNewPlant(view: View) {
+        val name = findViewById<EditText>(R.id.editTextTextPersonName).text.toString()
+        val family = findViewById<EditText>(R.id.editTextTextPersonName2).text.toString()
+        val dateOfPurchase = findViewById<EditText>(R.id.editTextDate).text.toString()
+        Log.i("anp", "addnewplant check : $name")
     }
 
 
