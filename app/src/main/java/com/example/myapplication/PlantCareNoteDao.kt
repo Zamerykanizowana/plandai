74506fa249b005f3a14d8223d71b3db68fd5e76d
Adam Olech <nddr89@gmail.com>
2021-09-25 16:36:06 +0200

    PlantCareNoteDao: add query by id and delete methods

--- app/src/main/java/com/example/myapplication/PlantCareNoteDao.kt
+++ app/src/main/java/com/example/myapplication/PlantCareNoteDao.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 
 
 @Dao
@@ -10,8 +11,14 @@ interface PlantCareNoteDao {
     @Query("SELECT * FROM plant_care_notes ORDER BY id ASC")
     suspend fun getNotes(): List<PlantCareNote>
 
+    @Query("SELECT * FROM plant_care_notes WHERE id=:noteId LIMIT 1")
+    fun getNoteById(noteId: Int): PlantCareNote
+
     @Insert
     suspend fun insert(plantCareNote: PlantCareNote) : Long
+
+    @Update
+    suspend fun update(plantCareNote: PlantCareNote)
 
     @Query("DELETE FROM plant_care_notes WHERE id= :noteId")
     suspend fun delete(noteId: Int)
