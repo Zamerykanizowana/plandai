ecb0fb23401eeb88e2acf044ddbecc8093805f7a
Aleksandra Wojewoda <a.wojewoda18@gmail.com>
2021-09-25 17:28:50 +0200

    AddNewPlantActivity: validation done

--- app/src/main/java/com/example/myapplication/AddNewPlantActivity.kt
+++ app/src/main/java/com/example/myapplication/AddNewPlantActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.app.DatePickerDialog
 import android.content.Intent
 import android.graphics.Color
+import android.graphics.PorterDuff
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -23,6 +24,7 @@ class AddNewPlantActivity : AppCompatActivity() {
 
     var cal = Calendar.getInstance()
 
+
     @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +33,7 @@ class AddNewPlantActivity : AppCompatActivity() {
         val dateField = findViewById<EditText>(R.id.editTextDate)
         dateField.setHint("Set date of purchase or planting --/--/--")
 
+        var onOpenSpinner = false
         // create an OnDateSetListener
         val dateSetListener = object : DatePickerDialog.OnDateSetListener {
             override fun onDateSet(view: DatePicker, year: Int, monthOfYear: Int,
@@ -41,6 +44,7 @@ class AddNewPlantActivity : AppCompatActivity() {
                 val myFormat = "dd/MM/yyyy" // mention the format you need
                 val sdf = SimpleDateFormat(myFormat, Locale.US)
                 dateField.setText(sdf.format(cal.getTime()))
+                setBagroundTintEditText(dateField, validationCheckEditText(dateField))
             }
         }
         // when you click on the button, show DatePickerDialog that is set with OnDateSetListener
@@ -87,22 +91,11 @@ class AddNewPlantActivity : AppCompatActivity() {
                     convertView,
                     parent
                 ) as TextView
-                // set item text bold
-//                view.setTypeface(view.typeface, Typeface.BOLD)
 
-                // set selected item style
-//                if (position == mySpinner.selectedItemPosition && position !=0 ){
-//                    view.background = ColorDrawable(Color.parseColor("#F7E7CE"))
-//                    view.setTextColor(Color.parseColor("#333399"))
-//                }
-
-                // make hint item color gray
                 if(position == 0){
                     view.setTextColor(Color.parseColor("#203700"))
                     view.setTextSize(TypedValue.COMPLEX_UNIT_SP, 25F)
                 }
-
-
                 return view
             }
 
@@ -113,10 +106,8 @@ class AddNewPlantActivity : AppCompatActivity() {
             }
         }
 
-
         // finally, data bind spinner with adapter
         mySpinner.adapter = adapter
-
 
         // spinner on item selected listener
         mySpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
@@ -126,37 +117,117 @@ class AddNewPlantActivity : AppCompatActivity() {
                 position: Int,
                 id: Long
             ) {
-                // nothing
+//                really stupid way to check if this is form's first open
+                if(!onOpenSpinner) {
+                    setBagroundTintSpinner(mySpinner, onOpenSpinner)
+                    onOpenSpinner = true
+                } else {
+                    setBagroundTintSpinner(mySpinner, validationCheckSpinner(mySpinner))
+                }
+
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
-                // another interface callback
+//                nothing
             }
         }
-
-
     }
 
 
     fun addNewPlant(view: View) {
-        val name = findViewById<EditText>(R.id.editTextTextPersonName).text.toString()
-        val family = findViewById<EditText>(R.id.editTextTextPersonName2).text.toString()
-        val dateOfPurchase = SimpleDateFormat("dd/MM/yyyy").parse(findViewById<EditText>(R.id.editTextDate).text.toString())
-        val size = findViewById<Spinner>(R.id.spinner1).selectedItem.toString()
-        Log.i("anp", "addnewplant check : $size")
-        val newPlant = Plant(0, name, family, dateOfPurchase, size)
-        val plantDb = PlantRoomDatabase.getDatabase(this)
+        val name = findViewById<EditText>(R.id.editTextTextPersonName)
+        val family = findViewById<EditText>(R.id.editTextTextPersonName2)
+//        val dateOfPurchase = SimpleDateFormat("dd/MM/yyyy").parse(findViewById<EditText>(R.id.editTextDate).text.toString())
+        val dateOfPurchase = findViewById<EditText>(R.id.editTextDate)
+        val size = findViewById<Spinner>(R.id.spinner1)
 
-        GlobalScope.launch(Dispatchers.Main) {
-            val newRow = plantDb.plantDao().insert(newPlant)
-            Log.i("id", "new plant : $newRow")
+        val sizeString = size.selectedItem.toString()
+        Log.w("check", "how sizeCheckWorks : $sizeString")
+
+        setBagroundTintEditText(name, validationCheckEditText(name))
+        setBagroundTintEditText(family, validationCheckEditText(family))
+        setBagroundTintEditText(dateOfPurchase, validationCheckEditText(dateOfPurchase))
+        setBagroundTintSpinner(size, validationCheckSpinner(size))
+
+        if (!validationCheckEditText(name) && !validationCheckEditText(family) && !validationCheckEditText(dateOfPurchase) && !validationCheckSpinner(size)) {
+            Log.i("check", "all editTexts are fulfil")
+            val newPlant = Plant(0, name.text.toString(), family.text.toString(), SimpleDateFormat("dd/MM/yyyy").parse(dateOfPurchase.text.toString()), size.selectedItem.toString())
+            val plantDb = PlantRoomDatabase.getDatabase(this)
+            GlobalScope.launch(Dispatchers.Main) {
+                val newRow = plantDb.plantDao().insert(newPlant)
+                Log.i("id", "new plant : $newRow")
 
 //            val toast = Toast.makeText(this@AddNewPlantActivity, "Added new plant with ID $newRow", Toast.LENGTH_SHORT)
-            val toast = Toast.makeText(this@AddNewPlantActivity, "Your new plant is on board!", Toast.LENGTH_SHORT)
+                val toast = Toast.makeText(this@AddNewPlantActivity, "Your new plant is on board!", Toast.LENGTH_SHORT)
+                toast.show()
+            }
+            goHome(view)
+        } else {
+            Log.i("check", "empty field in form")
+            val toast = Toast.makeText(this@AddNewPlantActivity, "Invalid value(s)!", Toast.LENGTH_SHORT)
             toast.show()
-        }
 
-        goHome(view)
+            name.onFocusChangeListener = View.OnFocusChangeListener { view, focus ->
+                if(validationCheckEditText(name) && !focus) {
+                    setBagroundTintEditText(name, true)
+                } else {
+                    setBagroundTintEditText(name, false)
+                }
+            }
+
+            family.onFocusChangeListener = View.OnFocusChangeListener { view, focus ->
+                if(validationCheckEditText(family) && !focus) {
+                    setBagroundTintEditText(family, true)
+                } else {
+                    setBagroundTintEditText(family, false)
+                }
+            }
+
+            dateOfPurchase.onFocusChangeListener = View.OnFocusChangeListener { view, focus ->
+                if(validationCheckEditText(dateOfPurchase)) {
+                    setBagroundTintEditText(dateOfPurchase, true)
+                } else {
+                    setBagroundTintEditText(dateOfPurchase, false)
+                }
+            }
+
+            size.onFocusChangeListener = View.OnFocusChangeListener { view, focus ->
+                if(validationCheckSpinner(size) && !focus) {
+                    setBagroundTintSpinner(size, true)
+                } else {
+                    setBagroundTintSpinner(size, false)
+                }
+            }
+
+        }
+    }
+
+    //DONE validation checking if selectedItem is NOT 0 element in spinner adapter data list (foolproof)
+    private fun validationCheckSpinner(spinner: Spinner): Boolean {
+//        return spinner.selectedItem.toString().compareTo("Select plant size") == 0
+        val checkFirstItem = spinner.adapter.getItem(0).toString()
+        Log.i("check", "check if taking data from adapter works : $checkFirstItem")
+        return spinner.selectedItem.toString().compareTo(spinner.adapter.getItem(0).toString()) == 0
+    }
+
+    private fun setBagroundTintSpinner(spinner: Spinner, boolean: Boolean) {
+        if (boolean){
+            spinner.getBackground().setColorFilter(resources.getColor(R.color.warning_red), PorterDuff.Mode.SRC_ATOP)
+        } else {
+            spinner.getBackground().setColorFilter(resources.getColor(R.color.black), PorterDuff.Mode.SRC_ATOP)
+        }
+    }
+
+    private fun validationCheckEditText(editText: EditText): Boolean {
+        return editText.text.toString().isNullOrEmpty()
+    }
+
+    private fun setBagroundTintEditText(editText: EditText, boolean: Boolean) {
+        if (boolean){
+            editText.getBackground().setColorFilter(resources.getColor(R.color.warning_red), PorterDuff.Mode.SRC_ATOP)
+        } else {
+            editText.getBackground().setColorFilter(resources.getColor(R.color.black), PorterDuff.Mode.SRC_ATOP)
+        }
     }
 
 
